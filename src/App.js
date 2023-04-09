9a0fe3dc33a9088186e97545741f13c7d723d068
Burak Ocak <ocakbura@gmail.com>
2023-04-09 16:13:21 +0300

    feat: ilk örnek fetch yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-
+import { useState, useEffect } from "react";
 
 function App() {
+
+  let [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+
+    /*
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => setDogImage(data.message))
+    */
+
+    //Yukarıdaki ile aşağıdaki kodun işlevleri arasında fark yoktur. Ancak aşağıdaki kodun okunaklılığı daha fazladır.
+    async function gorselCek(){
+      const yanit = await fetch("https://dog.ceo/api/breeds/image/random");
+      const veri = await yanit.json();
+
+      setDogImage(veri.message);
+    };
+    
+    gorselCek();
+
+  },[]);
+
   return (
-    <h2>App</h2>
+    <div>
+      {dogImage && <img src={dogImage} alt=""/>}
+    </div>
   );
 }
 
